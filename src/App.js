65bc2a45991643dde3cb59e293b5892853478ac0
favour anaonye <favouranaonye@gmail.com>
2026-09-16 19:58:56 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './Navbar';
 import Home from './home';
 
+
 function App() {
   return (
     <div className="App">
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,17 @@
+import { MdCameraAlt } from 'react-icons/md';
+import { FiMenu } from 'react-icons/fi';
+
 const Navbar = () => {
     return ( 
         <nav className="Navbar">
             <h1>Group Chat</h1>
-            <div className= "links">
-                <a href="/">camera</a>
-                <a href="/">Toggle menu</a>
+            <div className="Icons">
+                <button type="button" aria-label="Open camera">
+                    <MdCameraAlt size={24} color="white" />
+                </button>
+                <button type="button" aria-label="Toggle menu">
+                    <FiMenu size={24} color="white" />
+                </button>
             </div>
         </nav>
      );
