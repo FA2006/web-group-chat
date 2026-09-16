e0748716f6b72b3c4ce2f429bacdb550823127e6
favour anaonye <favouranaonye@gmail.com>
2026-09-16 11:06:37 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <div className="chat-group">
         <div className="chat-message">
           <h1>Hello, world!</h1>
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="Navbar">
+            <h1>Group Chat</h1>
+            <div className="links">
+                <a href="/">camera</a>
+                <a href="/">Toggle menu</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
